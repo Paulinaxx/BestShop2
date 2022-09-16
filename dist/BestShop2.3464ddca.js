a97ce12ce0355d5b738ccf8bb502054084d2c4db
Paulina <wojtowiczpaulina37@gmail.com>
2022-09-16 20:55:01 +0200

    from start

--- dist/BestShop2.3464ddca.js
+++ dist/BestShop2.3464ddca.js
@@ -142,7 +142,7 @@
       this[globalName] = mainExports;
     }
   }
-})({"c4xoB":[function(require,module,exports) {
+})({"1qXID":[function(require,module,exports) {
 "use strict";
 var global = arguments[3];
 var HMR_HOST = null;
@@ -533,6 +533,6 @@ function hmrAcceptRun(bundle, id) {
 
 },{}],"j4kuM":[function(require,module,exports) {
 
-},{}]},["c4xoB","j4kuM"], "j4kuM", "parcelRequire841d")
+},{}]},["1qXID","j4kuM"], "j4kuM", "parcelRequire841d")
 
 //# sourceMappingURL=BestShop2.3464ddca.js.map
